aaa978e817587682265ae6e34c8212bafc63352c
yanggaeng <carys3115@ajou.ac.kr>
2019-07-17 16:48:52 +0900

    initial configuration for pac_server
    (cherry picked from commit 1f453c77f602a35a1c96106c727968f36cda4eac)

---
+++ src/main/java/com/pac/PacServerApplication.java
@@ -0,0 +1,13 @@
+package com.pac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacServerApplication.class, args);
+	}
+
+}
