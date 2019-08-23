73122be0022d74786abf6303bd5392e8735bd43a
SeoYunKyung <carys3115@naver.com>
2019-08-23 10:25:08 +0900

    Revert "Sev user api"

--- src/main/java/com/pac/PacServerApplication.java
+++ src/main/java/com/pac/PacServerApplication.java
@@ -1,7 +1,5 @@
 package com.pac;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,10 +9,5 @@ public class PacServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PacServerApplication.class, args);
 	}
-	
-	@PostConstruct
-    public void init() {
-        
-    }
 
 }
