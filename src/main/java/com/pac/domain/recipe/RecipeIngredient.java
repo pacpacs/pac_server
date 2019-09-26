953a8f22311b276af5e8025641a88f52756353fd
yanggaeng <carys3115@ajou.ac.kr>
2019-09-26 16:30:15 +0900

    make as database schema

--- src/main/java/com/pac/domain/recipe/RecipeIngredient.java
+++ src/main/java/com/pac/domain/recipe/RecipeIngredient.java
@@ -1,4 +1,106 @@
 package com.pac.domain.recipe;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="recipe_ingredients")
 public class RecipeIngredient {
+    @Id
+    @JsonProperty("_id")
+    //@Indexed(unique = true)
+    private ObjectId _id;
+
+    @JsonProperty("irdntCpcty")
+    private String irdntCpcty;
+
+    @JsonProperty("irdntNm")
+    private String irdntNm;
+
+    @JsonProperty("irdntSn")
+    private int irdntSn;
+
+    @JsonProperty("rn")
+    private int rn;
+
+    @JsonProperty("irdntTyNm")
+    private String irdntTyNm;
+
+    @JsonProperty("recipeId")
+    private int recipeId;
+
+    @JsonProperty("irdntTyCode")
+    private String irdntTyCode;
+
+    public RecipeIngredient(ObjectId _id, String irdntCpcty, String irdntNm, int irdntSn, int rn, String irdntTyNm, int recipeId, String irdntTyCode) {
+        this._id = _id;
+        this.irdntCpcty = irdntCpcty;
+        this.irdntNm = irdntNm;
+        this.irdntSn = irdntSn;
+        this.rn = rn;
+        this.irdntTyNm = irdntTyNm;
+        this.recipeId = recipeId;
+        this.irdntTyCode = irdntTyCode;
+    }
+
+    public ObjectId get_id() {
+        return _id;
+    }
+
+    public String getIrdntCpcty() {
+        return irdntCpcty;
+    }
+
+    public void setIrdntCpcty(String irdntCpcty) {
+        this.irdntCpcty = irdntCpcty;
+    }
+
+    public String getIrdntNm() {
+        return irdntNm;
+    }
+
+    public void setIrdntNm(String irdntNm) {
+        this.irdntNm = irdntNm;
+    }
+
+    public int getIrdntSn() {
+        return irdntSn;
+    }
+
+    public void setIrdntSn(int irdntSn) {
+        this.irdntSn = irdntSn;
+    }
+
+    public int getRn() {
+        return rn;
+    }
+
+    public void setRn(int rn) {
+        this.rn = rn;
+    }
+
+    public String getIrdntTyNm() {
+        return irdntTyNm;
+    }
+
+    public void setIrdntTyNm(String irdntTyNm) {
+        this.irdntTyNm = irdntTyNm;
+    }
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public String getIrdntTyCode() {
+        return irdntTyCode;
+    }
+
+    public void setIrdntTyCode(String irdntTyCode) {
+        this.irdntTyCode = irdntTyCode;
+    }
 }
--- src/main/java/com/pac/domain/recipe/RecipeProcess.java
+++ src/main/java/com/pac/domain/recipe/RecipeProcess.java
@@ -1,4 +1,118 @@
 package com.pac.domain.recipe;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "recipe_process")
 public class RecipeProcess {
+    @Id
+    @JsonProperty("_id")
+    //@Indexed(unique = true)
+    private ObjectId _id;
+
+    @JsonProperty("streStepImageUrl")
+    private String streStepImageUrl;
+
+    @JsonProperty("stepTip")
+    private String stepTip;
+
+    @JsonProperty("rn")
+    private int rn;
+
+    @JsonProperty("recipeId")
+    private  int recipeId;
+
+    @JsonProperty("cookingDc")
+    private String cookingDc;
+
+    @JsonProperty("cookingNo")
+    private  int cookingNo;
+
+    public RecipeProcess(ObjectId _id, String streStepImageUrl, String stepTip, int rn, int recipeId, String cookingDc, int cookingNo) {
+        this._id = _id;
+        this.streStepImageUrl = streStepImageUrl;
+        this.stepTip = stepTip;
+        this.rn = rn;
+        this.recipeId = recipeId;
+        this.cookingDc = cookingDc;
+        this.cookingNo = cookingNo;
+    }
+
+    public RecipeProcess(String streStepImageUrl, String stepTip, int rn, int recipeId, String cookingDc, int cookingNo) {
+        this.streStepImageUrl = streStepImageUrl;
+        this.stepTip = stepTip;
+        this.rn = rn;
+        this.recipeId = recipeId;
+        this.cookingDc = cookingDc;
+        this.cookingNo = cookingNo;
+    }
+
+    public RecipeProcess(String streStepImageUrl, int rn, int recipeId, String cookingDc, int cookingNo) {
+        this.streStepImageUrl = streStepImageUrl;
+        this.rn = rn;
+        this.recipeId = recipeId;
+        this.cookingDc = cookingDc;
+        this.cookingNo = cookingNo;
+    }
+
+    public RecipeProcess(int rn, int recipeId, String cookingDc, int cookingNo) {
+        this.rn = rn;
+        this.recipeId = recipeId;
+        this.cookingDc = cookingDc;
+        this.cookingNo = cookingNo;
+    }
+
+    public ObjectId get_id() {
+        return _id;
+    }
+
+    public String getStreStepImageUrl() {
+        return streStepImageUrl;
+    }
+
+    public void setStreStepImageUrl(String streStepImageUrl) {
+        this.streStepImageUrl = streStepImageUrl;
+    }
+
+    public String getStepTip() {
+        return stepTip;
+    }
+
+    public void setStepTip(String stepTip) {
+        this.stepTip = stepTip;
+    }
+
+    public int getRn() {
+        return rn;
+    }
+
+    public void setRn(int rn) {
+        this.rn = rn;
+    }
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public String getCookingDc() {
+        return cookingDc;
+    }
+
+    public void setCookingDc(String cookingDc) {
+        this.cookingDc = cookingDc;
+    }
+
+    public int getCookingNo() {
+        return cookingNo;
+    }
+
+    public void setCookingNo(int cookingNo) {
+        this.cookingNo = cookingNo;
+    }
 }
