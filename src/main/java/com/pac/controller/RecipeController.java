147aaa650daf7b8681a80193163eb8e3ed6a7d5c
yanggaeng <carys3115@ajou.ac.kr>
2019-10-05 02:25:39 +0900

    controller get specific recipe

--- src/main/java/com/pac/controller/RecipeController.java
+++ src/main/java/com/pac/controller/RecipeController.java
@@ -1,7 +1,9 @@
 package com.pac.controller;
 
 import com.pac.domain.recipe.RecipeBasic;
+import com.pac.domain.recipe.RecipeBoard;
 import com.pac.domain.recipe.RecipeIngredient;
+import com.pac.domain.recipe.RecipeProcess;
 import com.pac.service.CommunityService;
 import com.pac.service.RecipeService;
 import org.slf4j.Logger;
@@ -10,11 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/recipes")
-public class RecipeController {
+public class RecipeController<RecipeAll> {
 
     Logger log = LoggerFactory.getLogger("com.pac.controller.RecipeController");
 
@@ -34,12 +38,16 @@ public class RecipeController {
         }
     }
 
+    @GetMapping("/specificRecipe")
+    public ResponseEntity<List<RecipeBoard>> getSepecificRecipe(@RequestParam("recipeNo") int recipeNo){
+
+        RecipeBasic recipeBasic= recipeService.getRecipesById(recipeNo);
+        List<RecipeProcess> recipeProcess = recipeService.getRecipeProcessById(recipeNo);
+        List<RecipeIngredient> recipeIngredient = recipeService.getRecipeIngredientsById(recipeNo);
+        return ResponseEntity.status(HttpStatus.OK).body(recipeService.getRecipeBoard(recipeBasic,recipeProcess,recipeIngredient));
+    }
 	@GetMapping("/getResult")
     public ResponseEntity<List<RecipeBasic>> getResutlByKeyword(@RequestParam("ingredients") List<String>ingredients){
-        for (String s :
-                ingredients) {
-            log.info(s);
-        }
         return ResponseEntity.status(HttpStatus.OK).body(recipeService.getRecipesByIngredients(ingredients));
     }
 
